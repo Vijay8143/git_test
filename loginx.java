5c153f18ed0a9242431127d33c270a876b5a8f5c
Vijay8143 <rationalworks81@gmail.com>
2025-09-16 23:43:22 +0530

    changes before

--- loginx.java
+++ loginx.java
@@ -1,5 +1,8 @@
 public class loginx {
     public static void main(String[] args) {
-        System.out.println("hello World");
+
+        System.out.println("Hello World");
+        System.out.println("i am vijay");
+
     }
 }
